51121250169058c1bf5d3b44bf033c389ede2935
amir lesani <xenups@outlook.com>
2020-02-10 10:38:25 +0330

    remove redundant import

--- runserver.py
+++ runserver.py
@@ -1,6 +1,5 @@
 import sys
 from manager.core_manager import execute_from_command_line, Core
-from pydoc import locate
 
 if __name__ == '__main__':
     address = execute_from_command_line(argv=sys.argv)
