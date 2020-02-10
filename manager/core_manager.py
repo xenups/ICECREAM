af719ad8f0403f188b4598c3058fb9a1afb32906
amir lesani <xenups@outlook.com>
2020-02-10 13:02:33 +0330

    fix outer scope of get subclasses

--- manager/core_manager.py
+++ manager/core_manager.py
@@ -1,4 +1,3 @@
-from abc import ABC, abstractmethod
 from pydoc import locate
 from bottle import Bottle, run
 from manager.baseapp import BaseApp
@@ -8,12 +7,6 @@ from settings import default_address, apps
 def get_default_address():
     _default = default_address
     return _default
-
-
-def get_subclasses(cls):
-    for subclass in cls.__subclasses__():
-        yield from get_subclasses(subclass)
-        yield subclass
 
 
 def execute_from_command_line(argv):
@@ -31,20 +24,26 @@ def execute_from_command_line(argv):
 class Core(object):
     def __init__(self, address):
         core = Bottle()
-        self.register_routers(core)
+        self.__register_routers(core)
         run(core, host=address['host'], port=address['port'])
 
     @staticmethod
-    def initialize_baseapps():
+    def __initialize_baseapps():
         try:
             for app in apps:
                 baseapp_class = locate(app)
                 instance = baseapp_class()
         except Exception as exception:
+            print('undefined app ')
             raise exception
 
-    def register_routers(self, core):
-        self.initialize_baseapps()
-        base_app_subclasses = get_subclasses(BaseApp)
+    def __get_subclasses(self, cls):
+        for subclass in cls.__subclasses__():
+            yield from self.__get_subclasses(subclass)
+            yield subclass
+
+    def __register_routers(self, core):
+        self.__initialize_baseapps()
+        base_app_subclasses = self.__get_subclasses(BaseApp)
         for sub_class in base_app_subclasses:
             sub_class.call_router(sub_class, core=core)
