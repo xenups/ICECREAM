c11b521b81351a8fe751377b276438635db5719e
amir lesani <xenups@outlook.com>
2020-02-25 15:20:42 +0330

    minor refactoring

--- app_foo/controller.py
+++ app_foo/controller.py
@@ -1,7 +1,12 @@
 "ICECREAM"
+import logging
+import sys
+
 from bottle import HTTPResponse, HTTPError
 from bottle_jwt import jwt_auth_required
 from marshmallow import Schema, fields, ValidationError
+
+from ICECREAM.wrappers import model_to_dict
 from app_foo.models import Room
 from app_foo.schemas import RoomSchema, room_serializer
 
@@ -10,23 +15,24 @@ def get_rooms(db_session):
     try:
         rooms = db_session.query(Room).all()
         serializer = RoomSchema(many=True)
-        result = serializer.dump(rooms)
-
         return rooms
-        # return HTTPResponse(status=200, body={'result': result})
     except Exception as e:
+        logging.error(e)
         raise HTTPError(status=400, body={'error': e.args.__str__()})
 
 
 def new_room(db_session, data):
     try:
-        room_serializer.load(data)
+        try:
+            room_serializer.load(data)
+        except ValidationError as err:
+            return err.messages
         room = Room()
         room.name = data['name']
         db_session.add(room)
         db_session.commit()
-        result = room_serializer.dump(db_session.query(Room).get(room.id))
-        # return HTTPResponse(status=200, body={'result': result})
+        # result = room_serializer.dump(db_session.query(Room).get(room.id))
+        result = db_session.query(Room).get(room.id)
         return result
-    except ValidationError as err:
-        return err.messages
+    except Exception as err:
+        raise err
