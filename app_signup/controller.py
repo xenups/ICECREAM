c5251e97372071f8a36fcb130bf3e89ec737c3d6
amir lesani <xenups@outlook.com>
2020-03-01 21:39:33 +0330

    sign up app refactored

--- app_signup/controller.py
+++ app_signup/controller.py
@@ -1,79 +1,56 @@
 "ICECREAM"
-import json
-import random
+
 from uuid import uuid4
-
 from bottle import HTTPError
-
-import redis
+from ICECREAM.cache import RedisCache
 from marshmallow import ValidationError
-
-from app_signup.repository.user_repository import check_user_exist_by_cell_number
 from app_signup.schemas import SMSSchema
+from ICECREAM.util import generate_otp_code
 from send_message.send_message import send_message
-
-redis_app = redis.Redis()
+from app_signup.repository.user_repository import user_by_cell_number
 
 valid_activating_interval = 86400
 valid_registering_interval = 200
 
 
-def activate_account(data, db_session):
-    print('hi')
-    # redis = get_redis()
-    # redis = redis_connector()
+def validation_account(data, db_session):
+    cache = RedisCache()
     cell_number = data.get('cell_no')
-    if check_user_exist_by_cell_number(cell_number, db_session):
+    if user_by_cell_number(cell_number, db_session):
         raise HTTPError(409, body='USER_ALREADY_EXISTS')
-    cell_data = redis_app.get(cell_number)
-    if cell_data is None:
+
+    activation_code = cache.get_cache_multiple_value(cell_number, 'activation_code')
+
+    if activation_code is None or activation_code != data.get('activation_code'):
         raise HTTPError(204, body='NO_VALID_ACTIVATION_CODE')
-
-    activation_code = (json.loads(cell_data.decode("utf-8"))).get(
-        'activation_code', None)
-
-    if activation_code is None:
-        raise HTTPError(204, body='NO_VALID_ACTIVATION_CODE')
-
-    if activation_code != data.get('activation_code'):
-        raise HTTPError(409, body='WRONG_ACTIVATION_CODE')
 
     signup_token = str(uuid4())
-    redis_app.delete(cell_number)
-    redis_app.set(cell_number, json.dumps({'signup_token': signup_token}),
-                  ex=valid_activating_interval)
+    cache.set_cache_multiple_value(cell_number, signup_token, 'signup_token', valid_activating_interval)
 
     data = {'cell_no': cell_number, 'signup_token': signup_token}
     return data
 
 
-def register(data, db_session):
+def activation_account(data, db_session):
     try:
         SMSSchema().load(data)
     except ValidationError as err:
         return err.messages
-    cell_no = data.get('cell_no')
-    if check_user_exist_by_cell_number(cell_no, db_session):
+    cache = RedisCache()
+    cell_number = data.get('cell_number')
+
+    if user_by_cell_number(cell_no=cell_number, db_session=db_session):
         raise HTTPError(409, body='USER_ALREADY_EXISTS')
-    cell_data = redis_app.get(cell_no)
 
-    if cell_data:
-        activation_code = (json.loads(cell_data.decode('utf-8'))).get(
-            'activation_code', None)
-        if activation_code:
-            raise HTTPError(403, ' Message.ALREADY_HAS_VALID_KEY')
-        else:
-            redis_app.delete(cell_no)
-    password = str(random.randint(1000, 9999))
-    data = {'receptor': cell_no,
-            'token': password,
-            'type': 'sms',
-            'template': 'ratingregister'}
-    sent_data = send_message(data)
-    print(sent_data)
+    if cache.get_cache_multiple_value(cell_number, 'activation_code'):
+        raise HTTPError(403, ' Message.ALREADY_HAS_VALID_KEY')
 
-    redis_app.set(cell_no, json.dumps({'activation_code': password}),
-                  ex=valid_registering_interval)
-    result = {'msg': ' Message.MESSAGE_SENT' + cell_no}
+    activation_code = generate_otp_code()
+    if send_message(cell_number=cell_number, activation_code=activation_code):
+        cache.set_cache_multiple_value(key=cell_number, value=activation_code, custom_value_name='activation_code',
+                                       ttl=valid_registering_interval)
+    else:
+        raise HTTPError(403, ' Message.Didnt send')
+
+    result = {'msg': ' Message.MESSAGE_SENT' + cell_number}
     return result
-
--- app_signup/urls.py
+++ app_signup/urls.py
@@ -1,10 +1,10 @@
 "ICECREAM"
 from ICECREAM.baseapp import BaseApp
 from ICECREAM.wrappers import db_handler, pass_data, jsonify
-from app_signup.controller import activate_account, register
+from app_signup.controller import validation_account, activation_account
 
 
 class SignUpApp(BaseApp):
     def call_router(self, core):
-        core.route('/register/activate-account', 'POST', activate_account, apply=[db_handler, pass_data, jsonify])
-        core.route('/register/send-code', 'POST', register, apply=[db_handler, pass_data, jsonify])
+        core.route('/register/validate-account', 'POST', validation_account, apply=[db_handler, pass_data, jsonify])
+        core.route('/register/activate-account', 'POST', activation_account, apply=[db_handler, pass_data, jsonify])
