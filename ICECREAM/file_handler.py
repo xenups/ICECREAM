befb02f35e1009edcd198832a3f94fc162e9529b
amir lesani <xenups@outlook.com>
2020-03-03 23:18:51 +0330

    file_handler.py added to upload files

---
+++ ICECREAM/file_handler.py
@@ -0,0 +1,22 @@
+import os
+import uuid
+import rootpath
+from settings import media_files
+
+rootpath.append()
+
+
+def upload(data, files_key='files'):
+    try:
+        files = data.get(files_key)
+        for file in files:
+            extension = os.path.splitext(file.filename)[1]
+            unique_file_name = str(uuid.uuid4()) + extension
+            file.filename = unique_file_name
+            save_path = os.path.join(media_files)
+            if not os.path.exists(save_path):
+                os.makedirs(save_path)
+            file.save(save_path)
+            return unique_file_name
+    except Exception as error:
+        raise error
