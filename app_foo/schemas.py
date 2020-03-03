4b1fdf04f0e35aabf09709d1c10d8c023394ede3
amir lesani <xenups@outlook.com>
2020-03-03 23:22:31 +0330

    a simple app added a model with image,its shows how to upload image

--- app_foo/schemas.py
+++ app_foo/schemas.py
@@ -7,4 +7,13 @@ class RoomSchema(Schema):
         fields = ('id', 'name')
 
 
+class RoomImageSchema(Schema):
+    name = fields.Str(required=True)
+    room_id = fields.Int(required=True)
+
+    class Meta:
+        fields = ('id', 'name', 'room_id', 'files')
+
+
 room_serializer = RoomSchema()
+room_image_serializer = RoomImageSchema()
