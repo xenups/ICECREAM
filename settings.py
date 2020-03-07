cbcb597b0aa647c82f7085ce45fc32bcdc40f3ed
amir lesani <xenups@outlook.com>
2020-03-07 04:05:20 +0330

    authentication defined in settings.py

--- settings.py
+++ settings.py
@@ -3,12 +3,15 @@ import os
 import rootpath
 from dotenv import load_dotenv, find_dotenv
 
+from app_user.authentication import AuthBackend
+
 rootpath.append()
 load_dotenv(find_dotenv())
 project_root = rootpath.detect()
 media_path = project_root + os.getenv('media_files')
 # you can using instead of os.getenv  media_files = /statics/media/
-DEBUG = True
+DEBUG: bool = True
+authentication = AuthBackend()
 apps = [
     'app_book.urls.BookApp',
     'app_foo.urls.FOOApp',
