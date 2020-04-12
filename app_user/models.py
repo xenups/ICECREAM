549756f64473a6c7b684ac48446764926321aa81
amir lesani <xenups@outlook.com>
2020-04-12 16:57:26 +0430

    tutorial added how to user role and rbac

--- app_user/models.py
+++ app_user/models.py
@@ -1,10 +1,8 @@
 "ICECREAM"
-from sqlalchemy import Column, String, ForeignKey, Boolean, Integer
-from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
+from ICECREAM.db_initializer import Base, ResourceMixin
+from sqlalchemy import Column, String, ForeignKey, Boolean, Integer
 from werkzeug.security import generate_password_hash, check_password_hash
-
-from ICECREAM.db_initializer import Base
 
 
 class Person(Base):
@@ -16,11 +14,12 @@ class Person(Base):
     bio = Column(String)
 
 
-class User(Base):
+class User(ResourceMixin, Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False, unique=True)
     password_hash = Column(String, nullable=False)
+    roles = Column(String, nullable=False, default="")
     person_id = Column(Integer, ForeignKey('persons.id'))
 
     person = relationship(Person, primaryjoin=person_id == Person.id, lazy=True)
@@ -30,3 +29,18 @@ class User(Base):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def get_roles(self):
+        return self.roles.split(",")
+
+    def set_roles(self, roles):
+        self.roles = ",".join(roles)
+
+
+class Message(ResourceMixin, Base):
+    """Message Model"""
+    __tablename__ = "post"
+    id = Column(Integer, primary_key=True)
+    content = Column(String, nullable=False)
+    owner_id = Column(ForeignKey(User.id), nullable=False)
+    owner = relationship(User, uselist=False, lazy="joined")
--- app_user/urls.py
+++ app_user/urls.py
@@ -1,10 +1,12 @@
 "ICECREAM"
 from ICECREAM.baseapp import BaseApp
+from bottle_jwt import jwt_auth_required
 from ICECREAM.wrappers import db_handler, pass_data, jsonify
-from app_user.controller import get_users, new_user
+from app_user.controller import get_users, new_user, new_message
 
 
 class USERApp(BaseApp):
     def call_router(self, core):
-        core.route('/getusers', 'GET', get_users, apply=[db_handler, jsonify])
+        core.route('/getusers', 'GET', get_users, apply=[db_handler, jsonify, jwt_auth_required])
         core.route('/adduser', 'POST', new_user, apply=[pass_data, db_handler, jsonify])
+        core.route('/createmessage', 'POST', new_message, apply=[pass_data, db_handler, jsonify, jwt_auth_required])
