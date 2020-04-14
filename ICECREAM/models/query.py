44472a08616b4d2f1099571fd5aa0f26a912fdd8
amir lesani <xenups@outlook.com>
2020-04-14 16:05:36 +0430

    get object or 404 added

--- ICECREAM/models/query.py
+++ ICECREAM/models/query.py
@@ -1,8 +1,10 @@
 #########need to be refactor################
+from ICECREAM.http import HTTPError
+
+
 def get_or_create(model, session, **kwargs):
     try:
         # basically check the obj from the db, this syntax might be wrong
-
         model_object = session.query(model).filter_by(**kwargs).first()
         if model_object is not None:
             return model_object
@@ -23,3 +25,15 @@ def get(model, session, **kwargs):
     except Exception as e:
         model_object = model()
         return model_object
+
+
+def get_object_or_404(model, session, **kwargs):
+    """
+    Use get() to return an object, or raise a Http404 exception if the object
+    does not exist.
+    """
+    try:
+        model_object = session.query(model).filter_by(**kwargs).first()
+        return model_object
+    except Exception as e:
+        raise HTTPError(404, body="Not Found!")
