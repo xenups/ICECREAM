a9a5a2ba5f365c92bbc9add94ee1c3ad85fa9bc0
amir lesani <xenups@outlook.com>
2020-04-14 17:31:08 +0430

    is_object_exist added to shortcuts queries

--- ICECREAM/models/query.py
+++ ICECREAM/models/query.py
@@ -1,10 +1,14 @@
+from sqlalchemy.orm.exc import NoResultFound
+
 #########need to be refactor################
 from ICECREAM.http import HTTPError
+from app_user.models import Person, User
 
 
 def get_or_create(model, session, **kwargs):
     try:
         # basically check the obj from the db, this syntax might be wrong
+
         model_object = session.query(model).filter_by(**kwargs).first()
         if model_object is not None:
             return model_object
@@ -27,13 +31,19 @@ def get(model, session, **kwargs):
         return model_object
 
 
-def get_object_or_404(model, session, **kwargs):
+def get_object_or_404(model, session, *args, **kwargs):
     """
     Use get() to return an object, or raise a Http404 exception if the object
     does not exist.
     """
-    try:
-        model_object = session.query(model).filter_by(**kwargs).first()
+    model_object = session.query(model).filter(*args, **kwargs).first()
+    if model_object:
         return model_object
-    except Exception as e:
-        raise HTTPError(404, body="Not Found!")
+    raise HTTPError(404, body="Not Found!")
+
+
+def is_object_exist(model, session, *args, **kwargs):
+    model_object = session.query(model).filter(*args, **kwargs).first()
+    if model_object:
+        raise HTTPError(409, body="Already exist")
+    return None
