a4f99df772a11d13bd139e457593837446f31314
amir lesani <xenups@outlook.com>
2020-04-17 22:35:26 +0430

    app user changed to phone instead of username superuser schema added

--- app_user/schemas.py
+++ app_user/schemas.py
@@ -1,20 +1,42 @@
 "ICECREAM"
 
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
+
+
+class SuperPersonSchema(Schema):
+    class Meta:
+        fields = ("id", "name", "last_name", 'bio')
+
+
+class SuperUserSchema(Schema):
+    person = fields.Nested(SuperPersonSchema)
+    id = fields.Integer(allow_none=True)
+    name = fields.String(required=True)
+    phone = fields.String(required=True, validate=[validate.Length(equal=11)])
+
+    class Meta:
+        fields = ("id", 'phone', 'password', 'roles', 'person')
 
 
 class PersonSchema(Schema):
     class Meta:
-        fields = ("id", "name", "last_name", 'phone', 'bio')
+        fields = ("id", "name", "last_name", 'bio')
 
 
 class UserSchema(Schema):
     person = fields.Nested(PersonSchema)
+    name = fields.String(required=True)
+    signup_token = fields.UUID(required=True)
+    phone = fields.String(required=True, validate=[validate.Length(equal=11)])
+    password = fields.Str(required=True, validate=[validate.Length(min=6, max=36)], load_only=True)
+
+    # roles =fields.String(required=True)
 
     class Meta:
-        fields = ('id', 'username', 'password', 'roles', 'person')
+        fields = ('id', 'signup_token', 'phone', 'password', 'roles', 'person')
 
 
-user_serializer = UserSchema()
+superuser_serializer = SuperUserSchema()
+user_serializer = UserSchema(only=('id', 'signup_token', 'phone', 'password', 'roles', 'person'))
 person_serializer = PersonSchema()
-users_serializer = UserSchema(many=True, only=('id', 'username', 'roles', 'person'))
+users_serializer = UserSchema(many=True, only=('id', 'signup_token', 'phone', 'roles', 'person'))
