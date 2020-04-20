ed8c1284e44120fba13b92f87d4536ee0ff02a14
amir lesani <xenups@outlook.com>
2020-04-20 21:33:38 +0430

    authentication field added to icecream unittest

--- ICECREAM/test.py
+++ ICECREAM/test.py
@@ -9,24 +9,32 @@ class APIResponse(object):
         self.status_code = None
         self.json = None
         self.body = None
+        self.content_type = None
 
 
 class Client(object):
     def __init__(self):
         self.test_core = TestApp(Core().execute_wsgi())
 
-    def get_api(self, api_url):
+    def get_api(self, api_url, *auth):
         response = APIResponse()
         __api_url = str(api_url)
+        if auth:
+            self.test_core.set_authorization(auth)
         response.json = json.dumps(self.test_core.get(__api_url).json)
-        response.status = self.test_core.get(__api_url).status
-        response.status_code = self.test_core.get(__api_url).status_code
-        response.body = self.test_core.get(__api_url).body
+        test_core_response = self.test_core.get(__api_url)
+        response.status = test_core_response.status
+        response.status_code = test_core_response.status_code
+        response.body = test_core_response.body
+        response.content_type = test_core_response.content_type
+
         return response
 
-    def post_api(self, api_url, data):
+    def post_api(self, api_url, data, auth=None):
         response = APIResponse()
         __api_url = str(api_url)
+        if auth:
+            self.test_core.set_authorization('JWT', auth)
         response.json = json.dumps(self.test_core.post_json(__api_url, params=data).json)
         response.status = self.test_core.post_json(__api_url, params=data).status
         response.status_code = self.test_core.post_json(__api_url, params=data).status_code
