25c50c7341c527f18869296cc3c3a9746892c756
amir lesani <xenups@outlook.com>
2020-04-21 20:16:08 +0430

    set_role removed

--- app_user/urls.py
+++ app_user/urls.py
@@ -1,13 +1,12 @@
 "ICECREAM"
 from ICECREAM.baseapp import BaseApp
 from bottle_jwt import jwt_auth_required
-from app_user.controller import get_users, new_user, new_message, set_role
+from app_user.controller import get_users, create_user, new_message
 from ICECREAM.wrappers import db_handler, pass_data, jsonify
 
 
 class USERApp(BaseApp):
     def call_router(self, core):
         core.route('/getusers', 'GET', get_users, apply=[db_handler, jsonify, jwt_auth_required])
-        core.route('/adduser', 'POST', new_user, apply=[pass_data, db_handler, jsonify])
-        core.route('/createmessage', 'POST', new_message, apply=[pass_data, db_handler, jsonify, jwt_auth_required])
-        core.route('/set_role', 'POST', set_role, apply=[pass_data, db_handler, jsonify, jwt_auth_required])
+        core.route('/adduser', 'POST', create_user, apply=[pass_data, db_handler, jsonify])
+        core.route('/createmessage', 'POST', new_message, apply=[pass_data, db_handler, jsonify])
