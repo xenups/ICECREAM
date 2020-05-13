18df31588101f240c6ef41237073aa0e787282a7
amir lesani <xenups@outlook.com>
2020-05-13 10:48:12 +0430

    authentication.py bug fix

--- app_user/authentication.py
+++ app_user/authentication.py
@@ -1,36 +1,54 @@
+from sqlalchemy.orm import Session
+from ICECREAM.http import HTTPError
+from ICECREAM.validators import validate_data
+from app_user.messages import USER_IS_NOT_ACTIVE, TOKEN_EXPIRED
 from app_user.models import User
-from ICECREAM.models.query import get_object
+from ICECREAM.models.query import  get_object
 from ICECREAM.wrappers import db_handler
 from bottle_jwt import JWTProviderPlugin
-from app_user.schemas import user_serializer
+from app_user.schemas import user_serializer, login_serializer
 from settings import project_secret, jwt_ttl
 
 
 class AuthBackend(object):
     @db_handler
-    def get_user_obj(self, db_session, phone):
+    def get_user_by_phone(self, db_session: Session, phone):
         user = get_object(User, db_session, User.phone == phone)
+        if not user:
+            raise HTTPError(200, "Unable to authenticate User", message="AuthError")
+        if not user.is_active:
+            raise HTTPError(*USER_IS_NOT_ACTIVE)
         return user
 
+    @db_handler
+    def get_user_by_id(self, db_session: Session, pk):
+        user = get_object(User, db_session, User.id == pk)
+        dumped_user = user_serializer.dump(user)
+        if not user:
+            raise HTTPError(*TOKEN_EXPIRED)
+        if not user.is_active:
+            raise HTTPError(*USER_IS_NOT_ACTIVE)
+        return dumped_user
+
     def authenticate_user(self, username, password):
-        user_obj = self.get_user_obj(phone=username)
+        validate_data(login_serializer, data={"phone": username, "password": password})
+        user_obj = self.get_user_by_phone(phone=username)
         if user_obj is not None:
             if username == user_obj.phone and user_obj.check_password(password):
-                self.user = user_serializer.dump(user_obj)
-                return self.user
-            return None
+                user = user_serializer.dump(user_obj)
+                return user
         return None
 
     def get_user(self, user_id):
-        if hasattr(self, 'user'):
-            if user_id == self.user['id']:
-                return {k: self.user[k] for k in self.user if k != 'password'}
+        user = self.get_user_by_id(pk=user_id)
+        if user_id == user['id']:
+            return {k: user[k] for k in user.keys() if k != 'password'}
         return None
 
 
 jwt_plugin = JWTProviderPlugin(
     keyword='jwt',
-    auth_endpoint='/auth',
+    auth_endpoint='/api/auth',
     backend=AuthBackend(),
     fields=('phone', 'password'),
     secret=project_secret,
