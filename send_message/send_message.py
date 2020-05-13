1eb8ee54e06d1d2da19ede9f1efdea0e30a0c266
amir lesani <xenups@outlook.com>
2020-05-13 11:30:41 +0430

    sms module fixed

--- send_message/send_message.py
+++ send_message/send_message.py
@@ -1,8 +1,7 @@
 import requests
-from bottle import HTTPError
 from kavenegar import *
-
 from settings import sms
+from ICECREAM.http import HTTPError
 
 # sms_token_url = sms['sms_token_url']
 # sms_security_key = sms['sms_security_key']
@@ -13,24 +12,44 @@ from settings import sms
 sms_api_key = sms['sms_api_key']
 
 
-def send_message(cell_number, activation_code):
-    data = {'receptor': cell_number,
-            'token': activation_code,
-            'type': 'sms',
-            'template': 'ratingregister'}
+class SMS(object):
+    def __init__(self):
+        self.api = KavenegarAPI(sms_api_key)
 
-    receptor = cell_number
-    if receptor.startswith('0999'):
-        data['receptor'] = '09210419379'
-    if sms_api_key is None:
-        raise HTTPError(400, 'MISSING_REQUIERED_FIELD')
-    try:
-        api = KavenegarAPI(sms_api_key)
-        params = data
-        response = api.verify_lookup(params)
-        print(response)
-    except APIException as e:
-        return {'status': 500}
-    except HTTPException as e:
-        return {'status': 500}
-    return {'status': 200}
+    def __send_sms(self, data):
+        receptor = data.get("receptor")
+        if receptor.startswith('0999'):
+            data['receptor'] = '09210419379'
+        if sms_api_key is None:
+            raise HTTPError(400, 'MISSING_REQUIERED_FIELD')
+        try:
+            params = data
+            response = self.api.verify_lookup(params)
+        except APIException as e:
+            return {'status': 500}
+        except HTTPException as e:
+            return {'status': 500}
+        return {'status': 200}
+
+    def send_activation_code(self, cell_number, activation_code):
+        try:
+            data = {'receptor': cell_number,
+                    'token': activation_code,
+                    'type': 'sms',
+                    'template': 'ratingregister'}
+            self.__send_sms(data)
+            return True
+        except Exception as error:
+            return False
+
+    def send_forget_password(self, cell_number, username, password):
+        try:
+            data = {'receptor': cell_number,
+                    'token': username,
+                    'token2': password,
+                    'type': 'sms',
+                    'template': 'fajrresetPassword'}
+            self.__send_sms(data)
+            return True
+        except Exception as error:
+            return False
