1817fe475d3070b4bc77542a990409efeecc118c
amir lesani <xenups@outlook.com>
2020-05-13 10:38:23 +0430

    HTTPResponse and error changed

--- ICECREAM/http.py
+++ ICECREAM/http.py
@@ -1,8 +1,9 @@
 import json
-from bottle import HTTPResponse
+
+from bottle import HTTPResponse as BottleHttpResponse
 
 
-class HTTPError(HTTPResponse):
+class HTTPResponse(BottleHttpResponse):
     default_status = 500
 
     def __init__(self, status=None, body=None, headers=None, exception=None, message="msg", traceback=None,
@@ -11,10 +12,14 @@ class HTTPError(HTTPResponse):
         self.traceback = traceback
         self.body = self.convert_to_json(body, message)
         headers = headers or {'Content-type': 'application/json'}
-        super(HTTPError, self).__init__(self.body, headers=headers, status=status, **options)
+        super(HTTPResponse, self).__init__(self.body, headers=headers, status=status, **options)
 
     @staticmethod
     def convert_to_json(body, message):
         if type(body) is dict:
             return json.dumps(body)
         return json.dumps({message: body})
+
+
+class HTTPError(HTTPResponse):
+    pass
