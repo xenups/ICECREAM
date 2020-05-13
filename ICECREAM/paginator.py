46c7829f410dc144ea123e074dfc54c899ff738a
amir lesani <xenups@outlook.com>
2020-05-13 10:39:21 +0430

    pagination request fixed into paginator

--- ICECREAM/paginator.py
+++ ICECREAM/paginator.py
@@ -1,3 +1,4 @@
+import bottle
 from marshmallow import Schema
 from ICECREAM.http import HTTPError
 from sqlalchemy_filters import apply_pagination
@@ -5,19 +6,22 @@ from sqlalchemy_filters import apply_pagination
 
 class PaginateSchema(Schema):
     class Meta:
-        fields = ("result","count", "next", "total_results", "total_page")
+        fields = ("result", "count", "next", "total_results", "total_page")
 
 
 class Paginate(object):
-    def __new__(cls, query, page_number=1, page_size=10, serializer=None):
-        query, pagination = apply_pagination(query, page_number=page_number,
-                                             page_size=page_size)
+    def __new__(cls, query, serializer=None):
+        page_number = bottle.request.GET.get('page') or 1
+        page_size = bottle.request.GET.get('count') or 10
+        query, pagination = apply_pagination(query, page_number=int(page_number),
+                                             page_size=int(page_size))
         page_size, page_number, num_pages, total_results = pagination
         _next = cls.is_next_page(page_number, page_size, total_results)
         result = serializer.dump(query)
+        __paginate_serializer = PaginateSchema()
         __dict_paginate = {"count": page_number, "next": _next, "total_results": total_results, "total_page": num_pages,
                            "result": result}
-        __serialized_result = PaginateSchema().dump(__dict_paginate)
+        __serialized_result = __paginate_serializer.dump(__dict_paginate)
         return __serialized_result
 
     @staticmethod
