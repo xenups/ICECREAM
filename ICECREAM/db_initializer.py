16db82c3b440e6df6d742a3a42cc3bdfafe77cd1
amir lesani <xenups@outlook.com>
2020-05-29 19:12:33 +0430

    MongoFilter class added to custom filtering in rest services

--- ICECREAM/db_initializer.py
+++ ICECREAM/db_initializer.py
@@ -1,8 +1,8 @@
 import logging
 from bottle import request
-from sqlalchemy import orm, create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy_searchable import sync_trigger, make_searchable
+from mongosql import MongoSqlBase
+from sqlalchemy import orm
+from sqlalchemy_searchable import make_searchable
 from sqlalchemy_utils import database_exists, create_database
 
 from settings import database
@@ -10,7 +10,7 @@ from sqla_wrapper import SQLAlchemy
 from sqlalchemy.ext.declarative import declarative_base
 
 logger = logging.getLogger()
-Base = declarative_base()
+Base = declarative_base(cls=(MongoSqlBase,))
 make_searchable(Base.metadata)
 
 
@@ -29,15 +29,6 @@ def get_database_uri():
 db = SQLAlchemy(get_database_uri(), pool_pre_ping=True)
 Base.metadata.create_all(db.engine)
 orm.configure_mappers()
-
-
-class ResourceMixin(object):
-
-    def __eq__(self, other):
-        return hasattr(other, "id") and self.id == other.id
-
-    def __hash__(self):
-        return hash(self.id)
 
 
 def get_db_session():
