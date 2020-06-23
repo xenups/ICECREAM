321769f3dc9a03322c3219c49d1ca419a14298ef
amir lesani <xenups@outlook.com>
2020-06-23 10:47:06 +0430

    auto commit added

--- ICECREAM/db_initializer.py
+++ ICECREAM/db_initializer.py
@@ -2,6 +2,8 @@ import logging
 from bottle import request
 from mongosql import MongoSqlBase
 from sqlalchemy import orm
+from sqlalchemy.orm import Session
+from sqlalchemy.pool import NullPool
 from sqlalchemy_searchable import make_searchable
 from sqlalchemy_utils import database_exists, create_database
 
@@ -18,23 +20,18 @@ def get_database_uri():
     uri = 'postgresql+psycopg2://{}:{}@{}:{}/{}'.format(database['db_user'], database['db_pass'],
                                                         database['db_host'], database['db_port'],
                                                         database['db_name'])
-    try:
-        if not database_exists(uri):
-            create_database(uri)
-    except Exception:
-        raise
     return uri
 
 
 db = SQLAlchemy(get_database_uri(), pool_pre_ping=True)
+db.session_options = {'autocommit': True}
 Base.metadata.create_all(db.engine)
 orm.configure_mappers()
 
 
-def get_db_session():
+def get_db_session() -> Session:
     if hasattr(request, 'db_session'):
         return request.db_session
     else:
-        orm.configure_mappers()
         request.db_session = db.session
         return request.db_session
