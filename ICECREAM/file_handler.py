312aa28b8a826b164b68cd2dc9cd11f385425548
amir lesani <xenups@outlook.com>
2020-07-19 12:05:31 +0430

    upload another file type added

--- ICECREAM/file_handler.py
+++ ICECREAM/file_handler.py
@@ -10,15 +10,15 @@ logger = logging.getLogger()
 rootpath.append()
 
 
-def upload(data, files_key='files'):
+def upload(data, files_key='files', image=True):
     try:
         files = data.get(files_key)
         for file in files:
             # extension = os.path.splitext(file.filename)[1]
             content_type = file.content_type
             extension = mimetypes.guess_extension(content_type)
-
-            check_image_extension(extension)
+            if image:
+                check_image_extension(extension)
             unique_file_name = str(uuid.uuid4()) + extension
             file.filename = unique_file_name
             save_path = os.path.join(media_path)
