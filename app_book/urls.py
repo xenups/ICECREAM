ff8ee2a67c061f593aa5aea796bab7b486494cab
amir lesani <xenups@outlook.com>
2020-12-11 02:53:48 +0330

    Fixed wrong routing for CRUD

--- app_book/urls.py
+++ app_book/urls.py
@@ -1,14 +1,14 @@
 from bottle_jwt2 import jwt_auth_required
 from ICECREAM.baseapp import BaseApp
-from ICECREAM.wrappers import pass_data
+from ICECREAM.wrappers import pass_data, debug
 from app_book.controller import get_authors, new_quote, get_author, get_quotes, get_quote, delete_quote
 
 
 class BookApp(BaseApp):
     def call_router(self, core):
-        core.route('/getauthors', 'GET', get_authors, apply=[jwt_auth_required])
-        core.route('/addquote', 'POST', new_quote, apply=[pass_data])
-        core.route('/getauthor/<pk>', 'GET', get_author)
-        core.route('/getquotes', 'GET', get_quotes)
-        core.route('/getquote/<pk>', 'GET', get_quote)
-        core.route('/deletequote/<pk>', 'GET', delete_quote)
+        core.route('/authors', 'GET', get_authors, apply=[jwt_auth_required, debug])
+        core.route('/quotes', 'POST', new_quote, apply=[pass_data])
+        core.route('/authors/<pk>', 'GET', get_author)
+        core.route('/quotes', 'GET', get_quotes)
+        core.route('/quotes/<pk>', 'GET', get_quote)
+        core.route('/quotes/<pk>', 'GET', delete_quote)
