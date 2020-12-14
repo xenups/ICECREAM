3c2dbf1dcb9a507d6916f0a88ec92ba7d230bece
amir lesani <xenups@outlook.com>
2020-12-15 02:40:22 +0330

    get_db_session() refactored

--- ICECREAM/db_initializer.py
+++ ICECREAM/db_initializer.py
@@ -65,8 +65,5 @@ orm.configure_mappers()
 
 
 def get_db_session() -> Session:
-    if hasattr(request, 'db_session'):
-        return request.db_session
-    else:
-        request.db_session = db.session
-        return request.db_session
+    setattr(request, 'db_session', db.session)
+    return getattr(request, 'db_session')
