55e852347b1b74a5233dc97f637cb95c86d668a0
amir lesani <xenups@outlook.com>
2020-12-18 03:12:45 +0330

    DBInjector Plugin has been refactored and cleaned

--- ICECREAM/plugin.py
+++ ICECREAM/plugin.py
@@ -3,7 +3,7 @@ import inspect
 import bottle
 from sqlalchemy.exc import SQLAlchemyError
 
-from ICECREAM.db_initializer import get_db_session
+from ICECREAM.db_initializer import DataBaseConnectionManager
 
 if not hasattr(bottle, 'PluginError'):
     class PluginError(bottle.BottleException):
@@ -17,8 +17,9 @@ class DBInjectorPlugin(object):
     name = 'sqlalchemy'
     api = 2
 
-    def __init__(self, keyword='db_session'):
+    def __init__(self, keyword='db_session', db_type='sqlite'):
         self.keyword = keyword
+        self.db_type = db_type
 
     def setup(self, app):
         for other in app.plugins:
@@ -30,29 +31,33 @@ class DBInjectorPlugin(object):
             elif other.name == self.name:
                 self.name += '_%s' % self.keyword
 
+    def inspect_parameters(self, callback):
+        try:
+            inspect.signature(callback)
+            # check if inspect.signature exists
+        except AttributeError:
+            argspec = inspect.getfullargspec(callback)
+            parameters = argspec.args
+            accept_kwargs = argspec.keywords
+        else:
+            parameters = inspect.signature(callback).parameters
+            accept_kwargs = any(p.kind == inspect.Parameter.VAR_KEYWORD
+                                for p in parameters.values())
+        return accept_kwargs, parameters
+
     def apply(self, callback, route):
         if bottle.__version__.startswith('0.9'):
             _callback = route['callback']
         else:
             _callback = route.callback
 
-        try:
-            inspect.signature
-            # check if inspect.signature exists
-        except AttributeError:
-            argspec = inspect.getfullargspec(_callback)
-            parameters = argspec.args
-            accept_kwargs = argspec.keywords
-        else:
-            parameters = inspect.signature(_callback).parameters
-            accept_kwargs = any(p.kind == inspect.Parameter.VAR_KEYWORD
-                                for p in parameters.values())
-
+        accept_kwargs, parameters = self.inspect_parameters(_callback)
         if not (accept_kwargs or self.keyword in parameters):
             return callback
 
         def wrapper(*args, **kwargs):
-            session = get_db_session()
+            db_manager = DataBaseConnectionManager(db_type=self.db_type)
+            session = db_manager.get_db_session()
             try:
                 session.expire_on_commit = True
                 kwargs['db_session'] = session
