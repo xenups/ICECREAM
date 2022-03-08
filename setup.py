99a1594691cc07a42ba592cd56983b0d12e61e9d
xenups <xenups@outlook.com>
2022-03-08 23:54:27 +0330

    setup version has been updated

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 setup(
     name='icecreamy',
-    version='0.0.6.4.1',
+    version='0.0.6.4.2',
     author='xenups',
     include_package_data=True,
     author_email="xenups@gmail.com",
